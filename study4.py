53a8e07882c48a458879c4be56147e25f7764114
Kenta Iwami <g2117009@fun.ac.jp>
2018-05-20 10:54:24 +0900

    ファイル構成を変更

--- study4.py
+++ study4.py
@@ -1,4 +1,7 @@
 from gensim.models.word2vec import Word2Vec
+import MeCab
+import numpy as np
+from scipy import spatial
 
 
 def word2vec():
@@ -14,5 +17,37 @@ def word2vec():
     for x in out:
         print(x[0], x[1])
 
+
+# def mecab_test():
+
+    # print(mecab.parse("彼は昨日、お腹を壊した"))
+
+
+def avg_feature_vector(sentence, model, num_features):
+    words = mecab.parse(sentence).replace(' \n', '').split() # mecabの分かち書きでは最後に改行(\n)が出力されてしまうため、除去
+    feature_vec = np.zeros((num_features,), dtype="float32") # 特徴ベクトルの入れ物を初期化
+    for word in words:
+        feature_vec = np.add(feature_vec, model[word])
+    if len(words) > 0:
+        feature_vec = np.divide(feature_vec, len(words))
+    return feature_vec
+
+
+def sentence_similarity(sentence_1, sentence_2):
+    # 今回使うWord2Vecのモデルは300次元の特徴ベクトルで生成されているので、num_featuresも300に指定
+    num_features=50
+    sentence_1_avg_vector = avg_feature_vector(sentence_1, word2vec_model, num_features)
+    sentence_2_avg_vector = avg_feature_vector(sentence_2, word2vec_model, num_features)
+    # １からベクトル間の距離を引いてあげることで、コサイン類似度を計算
+    return 1 - spatial.distance.cosine(sentence_1_avg_vector, sentence_2_avg_vector)
+
+
 if __name__ == '__main__':
-    word2vec()
+    mecab = MeCab.Tagger("-Owakati")
+    word2vec_model = Word2Vec.load('word2vec.gensim.model')
+
+    result = sentence_similarity(
+        "タスク管理について、ようやく着手することができた",
+        "タスク管理にようやく着手できた"
+    )
+    print(result)
