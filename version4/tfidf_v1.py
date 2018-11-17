f9a60897a55aae445f0abb0cfc7ea8938cd5b725
Kenta Iwami <g2117009@fun.ac.jp>
2018-11-18 00:04:43 +0900

    update tfidfのプログラム修正

--- version4/tfidf_v1.py
+++ version4/tfidf_v1.py
@@ -1,63 +1,50 @@
-import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
-from sklearn.feature_extraction import DictVectorizer
-import MeCab
+from gensim import corpora
+from gensim import models
+from pprint import pprint
+import math
+
+def main():
+    documents = ['a b c d',
+                 'c b c d a',
+                 'b b a',
+                 'c c',
+                 'c b a a a a a a a a d']
+
+    # gensim用に成形
+    texts = list(map(lambda x: x.split(), documents))
+
+    # 単語->id変換の辞書作成
+    dictionary = corpora.Dictionary(texts)
+    print('===単語->idの変換辞書===')
+    pprint(dictionary.token2id)
+
+    # textsをcorpus化
+    corpus = list(map(dictionary.doc2bow, texts))
+    print('===corpus化されたtexts===')
+    pprint(corpus)
+
+    # tfidf modelの生成
+    test_model = models.TfidfModel(corpus, wglobal=new_idf, normalize=False)
+    # corpusへのモデル適用
+    corpus_tfidf = test_model[corpus]
+
+    # id->単語へ変換
+    texts_tfidf = []  # id -> 単語表示に変えた文書ごとのTF-IDF
+    for doc in corpus_tfidf:
+        text_tfidf = []
+        for word in doc:
+            text_tfidf.append([dictionary[word[0]], word[1]])
+        texts_tfidf.append(text_tfidf)
+
+    # 表示
+    print('===結果表示===')
+    for text in texts_tfidf:
+        print(text)
 
 
-def test(text):
-    tagger = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd -Owakati")
-    tagger.parse('')
-    node = tagger.parseToNode(text).next
-    keywords = []
-    while node:
-        if node.feature.split(",")[0] == "名詞":
-            keywords.append(node.surface)
-        node = node.next
+def new_idf(docfreq, totaldocs, log_base=2.0, add=1.0):
+    return add + math.log(1.0 * totaldocs / docfreq, log_base)
 
-    return [x for x in keywords if x != '']
 
 if __name__ == '__main__':
-    f = open('2017/1015216.txt', 'r', encoding='utf-8')
-    sentences = f.readlines()
-    f.close()
-    sentences = [x.replace('\n', '') for x in sentences]
-
-    wakaties = []
-
-    for sent in sentences:
-        hoge = test(sent)
-        wakaties.append(' '.join(hoge))
-
-    count = CountVectorizer()
-    docs = np.array(wakaties)
-    bag = count.fit_transform(docs)
-
-    # 彼女という単語が配列の何番目に属しているか．
-    # print(count.vocabulary_.get('彼女'))
-
-    # tfidfの設定
-    tfidf = TfidfTransformer(use_idf=True, norm='l2', smooth_idf=True)
-    # 小数点第4位まで表示する．
-    np.set_printoptions(precision=4)
-    cnt_voc = count.vocabulary_
-    tfidf_array = np.array(tfidf.fit_transform(bag).toarray())
-
-    # ベクトルと単語を対応づけるために必要な処理．
-    vec = DictVectorizer()
-    vec.fit_transform(cnt_voc).toarray()
-    feature = vec.get_feature_names()
-
-    # tfidfの閾値の設定
-    threshold = 0.5
-    # ヒットした要素の数字を代入する．なぜか二つの配列で渡される．
-    hairetsu = np.where(tfidf_array >= threshold)
-    # np.uniqueで重複する値を削除する．
-    # なぜかnp.whereだと二つの配列が返ってくるので，連結して一つにする．
-    hit_word = np.unique(np.append(hairetsu[0], hairetsu[1]))
-
-    print(feature)
-
-    # tfidfで計算された単語を出力する．
-    for num in range(0, len(hit_word)):
-        print(feature[hit_word[num]])
+    main()
