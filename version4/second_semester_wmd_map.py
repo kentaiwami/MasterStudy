e01cfc4c3f37126acebb3b64a46177b61e363964
Kenta Iwami <g2117009@fun.ac.jp>
2018-11-19 13:42:06 +0900

    全てのドキュメントを保存する処理を実装

--- version4/second_semester_wmd_map.py
+++ version4/second_semester_wmd_map.py
@@ -1,6 +1,43 @@
+import MeCab
+from gensim.models import KeyedVectors
+import json
+
+
 def main():
-    pass
+    global document_id
+    file = open("../2018/後期.json")
+    data = json.load(file)
+    all_documents = []
+
+    for student_number in data.keys():
+        print(student_number)
+        for day in data[student_number].keys():
+
+            all_documents += add_document(data[student_number][day]['K'], student_number, day, 'K')
+            all_documents += add_document(data[student_number][day]['P'], student_number, day, 'P')
+            all_documents += add_document(data[student_number][day]['T'], student_number, day, 'T')
+
+
+
+def add_document(kpt_list, student_number, day, kpt):
+    global document_id
+    tmp_documents = []
+
+    for text in kpt_list:
+        tmp_documents.append({
+            'id': document_id,
+            'origin': text,
+            'student': student_number,
+            'day': day,
+            'KPT': kpt
+        })
+        document_id += 1
+
+    return tmp_documents
 
 
 if __name__ == '__main__':
+    document_id = 0
+    # mecab = MeCab.Tagger("-d /usr/local/lib/mecab/dic/mecab-ipadic-neologd -Owakati")
+    # word2vec_model = KeyedVectors.load_word2vec_format('../model/tohoku/model.bin', binary=True)
     main()
